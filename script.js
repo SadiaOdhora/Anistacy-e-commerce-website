aa4ecce3fcc06ad1cfbec4c31f9ec5e571409a2a
SadiaOdhora <107721542+SadiaOdhora@users.noreply.github.com>
2022-09-04 22:32:22 +0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const bar = document.getElementsById('bar');
-const nav = document.getElementsById('navbar');
-if(bar){
-  bar.addEventListener('click', () => {
-    nav.classList.add('active');
-  })
-}
